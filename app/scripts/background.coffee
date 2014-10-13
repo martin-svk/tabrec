e3e8bc465f10005aed2e385d11932c8de7140afe
Martin Toma <martin.toma.svk@gmail.com>
2014-10-13 18:02:57 +0200

    Move activate/disable settings to popup.js.

--- app/scripts/background.coffee
+++ app/scripts/background.coffee
@@ -1,14 +1,1 @@
 'use strict'
-
-active = true
-
-update_icon = () ->
-  if active
-    icon_name = 'active'
-  else
-    icon_name = 'inactive'
-  chrome.browserAction.setIcon({path:'../images/icon_' + icon_name + '.png'})
-  active = !active
-
-chrome.browserAction.onClicked.addListener(update_icon)
-update_icon()
--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,24 +1,5 @@
 (function() {
   'use strict';
-  var active, update_icon;
 
-  active = true;
-
-  update_icon = function() {
-    var icon_name;
-    if (active) {
-      icon_name = 'active';
-    } else {
-      icon_name = 'inactive';
-    }
-    chrome.browserAction.setIcon({
-      path: '../images/icon_' + icon_name + '.png'
-    });
-    return active = !active;
-  };
-
-  chrome.browserAction.onClicked.addListener(update_icon);
-
-  update_icon();
 
 }).call(this);
