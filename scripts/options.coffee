bc702f84141ff15207fa2689131bfbe1eb89ebc3
Martin Toma <martin.toma.svk@gmail.com>
2014-11-22 17:48:27 +0100

    Add new option (other_plugins).

--- scripts/options.coffee
+++ scripts/options.coffee
@@ -1,19 +1,21 @@
 'use strict'
 
-API_URL = 'http://tabber.fiit.stuba.sk'
-#API_URL = 'http://localhost:9292'
+#API_URL = 'http://tabber.fiit.stuba.sk'
+API_URL = 'http://localhost:9292'
 
 # Reset options method
 reset_options = () ->
   $('#user-level-select').val('beginner')
   $('#rec-mode-select').val('aggressive')
+  $('#other-plugins-cb').prop('checked', false)
 
 # Load options from chrome storage
 load_options = () ->
-  chrome.storage.sync.get ['user_level', 'rec_mode'], (result) ->
+  chrome.storage.sync.get ['user_level', 'rec_mode', 'other_plugins'], (result) ->
     if result.user_level and result.rec_mode
       $('#user-level-select').val(result.user_level)
       $('#rec-mode-select').val(result.rec_mode)
+      $('#other-plugins-cb').prop('checked', result.other_plugins)
     else
       reset_options()
 
@@ -21,20 +23,23 @@ load_options = () ->
 save_option = () ->
   userLevel = $('#user-level-select').val()
   recMode = $('#rec-mode-select').val()
+  otherPlugins = $('#other-plugins-cb').prop('checked')
   chrome.storage.sync.set
+    'other_plugins': otherPlugins
     'user_level': userLevel
     'rec_mode': recMode, ->
       chrome.storage.sync.get ['user_id'], (result) ->
-        update_user(result.user_id, userLevel, recMode)
+        update_user(result.user_id, userLevel, recMode, otherPlugins)
 
 # Update user record on server
-update_user = (id, exp, rec) ->
+update_user = (id, exp, rec, op) ->
   $.ajax "#{API_URL}/users/#{id}",
     type: 'PUT'
     dataType: 'json'
     data: { user: {
       rec_mode: rec
       experience: exp
+      other_plugins: op
     } }
     success: (data, textStatus, jqXHR) ->
       swal('Success!', 'Your settings has been saved!', 'success')
--- scripts/options.js
+++ scripts/options.js
@@ -3,18 +3,20 @@
   'use strict';
   var API_URL, load_options, reset_options, save_option, update_user;
 
-  API_URL = 'http://tabber.fiit.stuba.sk';
+  API_URL = 'http://localhost:9292';
 
   reset_options = function() {
     $('#user-level-select').val('beginner');
-    return $('#rec-mode-select').val('aggressive');
+    $('#rec-mode-select').val('aggressive');
+    return $('#other-plugins-cb').prop('checked', false);
   };
 
   load_options = function() {
-    return chrome.storage.sync.get(['user_level', 'rec_mode'], function(result) {
+    return chrome.storage.sync.get(['user_level', 'rec_mode', 'other_plugins'], function(result) {
       if (result.user_level && result.rec_mode) {
         $('#user-level-select').val(result.user_level);
-        return $('#rec-mode-select').val(result.rec_mode);
+        $('#rec-mode-select').val(result.rec_mode);
+        return $('#other-plugins-cb').prop('checked', result.other_plugins);
       } else {
         return reset_options();
       }
@@ -22,27 +24,30 @@
   };
 
   save_option = function() {
-    var recMode, userLevel;
+    var otherPlugins, recMode, userLevel;
     userLevel = $('#user-level-select').val();
     recMode = $('#rec-mode-select').val();
+    otherPlugins = $('#other-plugins-cb').prop('checked');
     return chrome.storage.sync.set({
+      'other_plugins': otherPlugins,
       'user_level': userLevel,
       'rec_mode': recMode
     }, function() {
       return chrome.storage.sync.get(['user_id'], function(result) {
-        return update_user(result.user_id, userLevel, recMode);
+        return update_user(result.user_id, userLevel, recMode, otherPlugins);
       });
     });
   };
 
-  update_user = function(id, exp, rec) {
+  update_user = function(id, exp, rec, op) {
     return $.ajax("" + API_URL + "/users/" + id, {
       type: 'PUT',
       dataType: 'json',
       data: {
         user: {
           rec_mode: rec,
-          experience: exp
+          experience: exp,
+          other_plugins: op
         }
       },
       success: function(data, textStatus, jqXHR) {
