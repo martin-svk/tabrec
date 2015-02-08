b712b5f63aeb307ab8c21159c192c2cc4d0394da
Martin Toma <martin.toma.svk@gmail.com>
2015-02-08 12:33:40 +0100

    Back to included css files bcoz of chrome extension policy.

--- bin/dist.sh
+++ bin/dist.sh
@@ -21,12 +21,21 @@ cp -r ./images/ ./dist/images/
 echo "Copying locale files."
 cp -r ./_locales/ ./dist/_locales/
 
-echo "Copying manifest files. Don't forget to update version!"
-cp ./manifest.json ./dist/
-
 echo "Copying jquery files."
 mkdir -p ./dist/bower_components/jquery/dist/
 cp ./bower_components/jquery/dist/jquery.min.js ./dist/bower_components/jquery/dist/
 cp ./bower_components/jquery/dist/jquery.min.map ./dist/bower_components/jquery/dist/
 
-echo "For bootstrap and sweetalert use CDNs."
+echo "Copying bootstrap js and css files."
+mkdir -p ./dist/bower_components/bootstrap/dist/css/
+cp ./bower_components/bootstrap/dist/css/bootstrap.min.css ./dist/bower_components/bootstrap/dist/css/
+mkdir -p ./dist/bower_components/bootstrap/dist/js/
+cp ./bower_components/bootstrap/dist/js/bootstrap.min.js ./dist/bower_components/bootstrap/dist/js/
+
+echo "Copying sweetalert files."
+mkdir -p ./dist/bower_components/sweetalert/lib/
+cp ./bower_components/sweetalert/lib/sweet-alert.min.js ./dist/bower_components/sweetalert/lib/
+cp ./bower_components/sweetalert/lib/sweet-alert.css ./dist/bower_components/sweetalert/lib/
+
+echo "Copying manifest files. Don't forget to update version!"
+cp ./manifest.json ./dist/
