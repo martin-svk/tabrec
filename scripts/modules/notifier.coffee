fa027ba5b15d2fd1d75b0e162f9b2391c1f9e99d
Martin Toma <martin.toma.svk@gmail.com>
2015-03-31 19:29:37 +0200

    Implement basic notification show with accept/reject buttons.

--- scripts/modules/notifier.coffee
+++ scripts/modules/notifier.coffee
@@ -9,9 +9,38 @@
 
 class @Notifier
   debug_mode = null
+  tab_sort_notification_options =
+    type: 'basic'
+    iconUrl: 'images/notification.png'
+    title: 'Pattern detected!'
+    message: 'Looks like you are looking for some tabs, need help?'
+    buttons: [
+        title: 'Accept'
+        iconUrl: 'images/accept.png'
+      ,
+        title: 'Reject'
+        iconUrl: 'images/reject.png'
+    ]
 
   constructor: () ->
     debug_mode = Constants.is_debug_mode()
+    chrome.notifications.onButtonClicked.addListener(notification_button_clicked)
+    chrome.notifications.onClicked.addListener(notification_clicked)
 
   notify: (pattern) ->
     console.log("Notification: pattern occured: #{pattern}") if debug_mode
+    chrome.notifications.create("notification_#{new Date().getTime()}", tab_sort_notification_options, (id) -> )
+
+  # ===================================
+  # Event handlers
+  # ===================================
+
+  notification_button_clicked = (notif_id, button_index) ->
+    if button_index == 0
+      console.log('recommendation accepted') if debug_mode
+    else if button_index == 1
+      console.log('recommendation rejected') if debug_mode
+
+  notification_clicked = (notif_id) ->
+    console.log('recommendation accepted') if debug_mode
+
--- scripts/modules/notifier.js
+++ scripts/modules/notifier.js
@@ -2,17 +2,54 @@
 (function() {
   'use strict';
   this.Notifier = (function() {
-    var debug_mode;
+    var debug_mode, notification_button_clicked, notification_clicked, tab_sort_notification_options;
 
     debug_mode = null;
 
+    tab_sort_notification_options = {
+      type: 'basic',
+      iconUrl: 'images/notification.png',
+      title: 'Pattern detected!',
+      message: 'Looks like you are looking for some tabs, need help?',
+      buttons: [
+        {
+          title: 'Accept',
+          iconUrl: 'images/accept.png'
+        }, {
+          title: 'Reject',
+          iconUrl: 'images/reject.png'
+        }
+      ]
+    };
+
     function Notifier() {
       debug_mode = Constants.is_debug_mode();
+      chrome.notifications.onButtonClicked.addListener(notification_button_clicked);
+      chrome.notifications.onClicked.addListener(notification_clicked);
     }
 
     Notifier.prototype.notify = function(pattern) {
       if (debug_mode) {
-        return console.log("Notification: pattern occured: " + pattern);
+        console.log("Notification: pattern occured: " + pattern);
+      }
+      return chrome.notifications.create("notification_" + (new Date().getTime()), tab_sort_notification_options, function(id) {});
+    };
+
+    notification_button_clicked = function(notif_id, button_index) {
+      if (button_index === 0) {
+        if (debug_mode) {
+          return console.log('recommendation accepted');
+        }
+      } else if (button_index === 1) {
+        if (debug_mode) {
+          return console.log('recommendation rejected');
+        }
+      }
+    };
+
+    notification_clicked = function(notif_id) {
+      if (debug_mode) {
+        return console.log('recommendation accepted');
       }
     };
 
