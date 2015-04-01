57292127e4c6e215e7bb788140bf9fe9ce9f5b9d
Martin Toma <martin.toma.svk@gmail.com>
2015-04-01 14:56:18 +0200

    Update compile script, check for tools to exists. Add pandoc to compile changelog to html.

--- bin/compile.sh
+++ bin/compile.sh
@@ -3,8 +3,30 @@
 # Break on error
 set -e
 
-echo "Compiling coffee scripts."
-coffee -c ./scripts
+command_exists () {
+  type "$1" &> /dev/null;
+}
 
-echo "Compiling sass stylesheets."
-sass --update ./styles --style compressed
+if command_exists coffee; then
+  echo "Compiling coffee scripts."
+  coffee -c ./scripts
+else
+  echo "Can't compile coffee scripts, you need coffeescript compiler to continue!"
+  exit
+fi
+
+if command_exists sass; then
+  echo "Compiling sass stylesheets."
+  sass --update ./styles --style compressed
+else
+  echo "Can't compile sass styles, you need sass compiler to continue!"
+  exit
+fi
+
+if command_exists pandoc; then
+  echo "Converting changelog to HTML."
+  pandoc CHANGELOG.md -f markdown -t html -s -o ./pages/CHANGELOG.html
+else
+  echo "Can't convert changelog to HTML, you need pandoc to continue!"
+  exit
+fi
