e0ebbf542db677abfe6271bbc230537e6680a04c
Martin Toma <martin.toma.svk@gmail.com>
2015-04-01 18:49:59 +0200

    Add also recommendation stats to popup window.

--- scripts/popup.coffee
+++ scripts/popup.coffee
@@ -5,22 +5,29 @@ API_URL = Constants.get_api_url()
 $ ->
   chrome.storage.sync.get ['user_id'], (result) ->
     if result.user_id
+      # Browsing stats
       $.ajax "#{API_URL}/stats/browsing/#{result.user_id}",
         type: 'GET'
         dataType: 'json'
         success: (data, textStatus, jqXHR) ->
-          $('#w-tabs-created').html(data.weekly.created)
-          $('#w-tabs-closed').html(data.weekly.closed)
           $('#tabs-created').html(data.alltime.created)
           $('#tabs-closed').html(data.alltime.closed)
         error: (jqXHR, textStatus, errorThrown) ->
-          $('#w-tabs-created').html('N/A')
-          $('#w-tabs-closed').html('N/A')
           $('#tabs-created').html('N/A')
           $('#tabs-closed').html('N/A')
 
-    $('#settings').on 'click', ->
-      chrome.tabs.create {
-        url: chrome.runtime.getURL('options.html')
-        active: true
-      }
+      # Rec stats
+      $.ajax "#{API_URL}/stats/rec/#{result.user_id}",
+        type: 'GET'
+        dataType: 'json'
+        success: (data, textStatus, jqXHR) ->
+          $('#recs-accepted').html(data.accepted)
+          $('#recs-rejected').html(data.rejected)
+        error: (jqXHR, textStatus, errorThrown) ->
+          $('#recs-accepted').html('N/A')
+          $('#recs-rejected').html('N/A')
+
+  $('#settings').on 'click', ->
+    chrome.tabs.create
+      url: chrome.runtime.getURL('options.html')
+      active: true
--- scripts/popup.js
+++ scripts/popup.js
@@ -6,30 +6,38 @@
   API_URL = Constants.get_api_url();
 
   $(function() {
-    return chrome.storage.sync.get(['user_id'], function(result) {
+    chrome.storage.sync.get(['user_id'], function(result) {
       if (result.user_id) {
         $.ajax("" + API_URL + "/stats/browsing/" + result.user_id, {
           type: 'GET',
           dataType: 'json',
           success: function(data, textStatus, jqXHR) {
-            $('#w-tabs-created').html(data.weekly.created);
-            $('#w-tabs-closed').html(data.weekly.closed);
             $('#tabs-created').html(data.alltime.created);
             return $('#tabs-closed').html(data.alltime.closed);
           },
           error: function(jqXHR, textStatus, errorThrown) {
-            $('#w-tabs-created').html('N/A');
-            $('#w-tabs-closed').html('N/A');
             $('#tabs-created').html('N/A');
             return $('#tabs-closed').html('N/A');
           }
         });
-      }
-      return $('#settings').on('click', function() {
-        return chrome.tabs.create({
-          url: chrome.runtime.getURL('options.html'),
-          active: true
+        return $.ajax("" + API_URL + "/stats/rec/" + result.user_id, {
+          type: 'GET',
+          dataType: 'json',
+          success: function(data, textStatus, jqXHR) {
+            $('#recs-accepted').html(data.accepted);
+            return $('#recs-rejected').html(data.rejected);
+          },
+          error: function(jqXHR, textStatus, errorThrown) {
+            $('#recs-accepted').html('N/A');
+            return $('#recs-rejected').html('N/A');
+          }
         });
+      }
+    });
+    return $('#settings').on('click', function() {
+      return chrome.tabs.create({
+        url: chrome.runtime.getURL('options.html'),
+        active: true
       });
     });
   });
